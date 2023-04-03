2f2c01c872ba4b2af0a337b9da90fb7a63831ae0
SurriyaLuavan <surriyaluavan@gmail.com>
2023-04-03 20:56:59 -0300

    Add react component structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import "./App.css";
+import "./styles/App.css";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Skills from "./components/Skills";
+import Qualification from "./components/Qualification";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
-  return <div>Personal Portfolio</div>;
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Skills />
+        <Qualification />
+        <Portfolio />
+        <Contact />
+      </main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/cards/QualificationCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QualificationCard = () => {
+  return <div></div>;
+};
+
+export default QualificationCard;
