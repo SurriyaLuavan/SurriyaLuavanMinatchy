04e1b45740fbb7bf1baf3e77b6a0ade5e9a10388
SurriyaLuavan <surriyaluavan@gmail.com>
2023-04-04 09:12:08 -0300

    Add hero section markup and assets

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,22 @@
 import React from "react";
+import myPhoto from "../assets/myPhoto.png";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <section>
+      <h1>Hi There! I'm Surriya Luavan Minatchy</h1>
+      <p>
+        A dedicated frontend web developer who strives to create beautiful and
+        intuitive user experiences through clean, efficient, and maintainable
+        code.
+      </p>
+      <button type="button">
+        {" "}
+        <a href="#projects">VIEW PORTFOLIO</a>{" "}
+      </button>
+      <img src={myPhoto} alt="surriya" width="700px" />
+    </section>
+  );
 };
 
 export default Hero;
