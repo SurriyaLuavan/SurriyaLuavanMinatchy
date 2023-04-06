029974ba8e6bf03a21242a77a29feb33c021e129
SurriyaLuavan <surriyaluavan@gmail.com>
2023-04-06 07:12:57 -0300

    Style hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,26 @@
 import React from "react";
 import myPhoto from "../assets/myPhoto.png";
+import "../styles/Hero.css";
 
 const Hero = () => {
   return (
-    <section id="hero">
-      <h1 className="uppercase fs-primary-subheading fw-semi-bold">
-        Hi There! I'm <br />{" "}
-        <span className="uppercase-remove fs-primary-heading fw-bold">
-          Surriya Luavan Minatchy
-        </span>
-      </h1>
-      <p className="fs-primary-body fw-regular">
-        A dedicated frontend web developer who strives to create beautiful and
-        intuitive user experiences through clean, efficient, and maintainable
-        code.
-      </p>
-      <button type="button" className="uppercase btn ">
-        <a href="#projects">view projects</a>
-      </button>
+    <section id="hero" className="hero-container">
+      <div className="text-container">
+        <h1 className="uppercase fs-primary-subheading fw-semi-bold">
+          Hi There! I'm <br />{" "}
+          <span className="uppercase-remove fs-primary-heading fw-bold">
+            Surriya Luavan Minatchy
+          </span>
+        </h1>
+        <p className="fs-primary-body fw-regular">
+          A dedicated frontend web developer who strives to create beautiful and
+          intuitive user experiences through clean, efficient, and maintainable
+          code.
+        </p>
+        <button type="button" className="uppercase btn ">
+          <a href="#projects">view projects</a>
+        </button>
+      </div>
       <img src={myPhoto} alt="surriya" width="700px" />
     </section>
   );
