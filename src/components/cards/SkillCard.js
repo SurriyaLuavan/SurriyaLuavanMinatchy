eaded0322f0d8f033d6a4015d21f3d5901aa7cba
SurriyaLuavan <surriyaluavan@gmail.com>
2023-04-06 22:30:45 -0300

    Style skills component and skill card

--- src/components/cards/SkillCard.js
+++ src/components/cards/SkillCard.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "../../styles/SkillCard.css";
+import { useTheme } from "../../context/ThemeProvider";
 
 const SkillCard = ({ card }) => {
+  const { theme } = useTheme();
   return (
-    <article>
-      <img src={card.logo} alt={`${card.title} logo`} width="75px" />
-      <h3 className="uppercase fs-secondary-body fw-semi-bold">{card.title}</h3>
+    <article className={`card bg-secondary ${theme === "light" && "light"}`}>
+      <img src={card.logo} alt={`${card.title} logo`} className="skillLogo" />
+      <h3 className="uppercase fs-primary-body fw-semi-bold">{card.title}</h3>
     </article>
   );
 };
