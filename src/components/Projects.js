b4421f08be71f6e9a38bae9c926b1a026d7bd002
SurriyaLuavan <surriyaluavan@gmail.com>
2023-04-07 21:19:18 -0300

    Style project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,71 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { projects } from "../data/projects";
 import ProjectCard from "../components/cards/ProjectCard";
 import { v4 as uuidv4 } from "uuid";
+import "../styles/Projects.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import { register } from "swiper/element/bundle";
+
+register();
 
 const Projects = () => {
+  const swiperElRef = useRef(null);
+
+  // useEffect(() => {
+  //   // listen for Swiper events using addEventListener
+  //   swiperElRef.current.addEventListener("progress", (e) => {
+  //     const [swiper, progress] = e.detail;
+  //     console.log(progress);
+  //   });
+
+  //   swiperElRef.current.addEventListener("slidechange", (e) => {
+  //     console.log("slide changed");
+  //   });
+  // }, []);
   return (
     <section
       id="Projects"
-      style={{ paddingTop: "10rem" }}
-      className="padding-inline"
+      className="projects-container | padding-inline padding-block"
     >
-      <p className="uppercase fs-secondary-subheading fw-semi-bold">
-        Some of my recent work
-      </p>
-      <h2 className="fs-secondary-heading fw-bold text-accent">Projects</h2>
-      <ul>
-        {projects.map((project) => {
-          return <ProjectCard key={uuidv4()} projectInfo={project} />;
-        })}
-      </ul>
+      <div class="projects-wrapper">
+        <div class="projects-title">
+          <p className="uppercase fs-secondary-subheading fw-semi-bold">
+            Some of my recent work
+          </p>
+          <h2 className="fs-secondary-heading fw-bold text-accent">Projects</h2>
+        </div>
+        {/* <Swiper
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper> */}
+        {/* <swiper-container
+          ref={swiperElRef}
+          slides-per-view="3"
+          speed="500"
+          loop="true"
+          css-mode="true"
+        >
+          <swiper-slide>Slide 1</swiper-slide>
+          <swiper-slide>Slide 2</swiper-slide>
+          <swiper-slide>Slide 3</swiper-slide>
+          ...
+        </swiper-container> */}
+        <ul className="project-card-container">
+          {projects.map((project) => {
+            return (
+              <SwiperSlide>
+                <ProjectCard key={uuidv4()} projectInfo={project} />
+              </SwiperSlide>
+            );
+          })}
+        </ul>
+      </div>
     </section>
   );
 };
